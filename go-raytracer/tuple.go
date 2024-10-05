abb839176095bc8ac62ca560667e0a6c3f98428f
thecatblake <k222ryousuke@gmail.com>
2024-10-06 02:01:40 +0900

    add magnitude

--- go-raytracer/tuple.go
+++ go-raytracer/tuple.go
@@ -1,16 +1,42 @@
 package goraytracer
 
+import "math"
+
 type Tuple struct {
-	x	float32
-	y	float32
-	z	float32
-	w	float32
+	x	float64
+	y	float64
+	z	float64
+	w	float64
 }
 
-func Point(x float32, y float32, z float32) Tuple {
+func Point(x float64, y float64, z float64) Tuple {
 	return Tuple{x, y, z, 1.0};
 }
 
-func Vector(x float32, y float32, z float32) Tuple {
+func Vector(x float64, y float64, z float64) Tuple {
 	return Tuple{x, y, z, 0.0};
+}
+
+func (left Tuple) Add(right Tuple) Tuple {
+	return Tuple{left.x + right.x, left.y + right.y, left.z + right.z, left.w + right.w}
+}
+
+func (left Tuple) Sub(right Tuple) Tuple {
+	return Tuple{left.x - right.x, left.y - right.y, left.z - right.z, left.w - right.w}
+}
+
+func (t Tuple) Neg() Tuple {
+	return Tuple{-t.x, -t.y, -t.z, -t.w}
+}
+
+func (t Tuple) Mul(k float64) Tuple {
+	return Tuple{t.x * k, t.y * k, t.z * k, t.w * k}
+}
+
+func (t Tuple) Div(k float64) Tuple {
+	return Tuple{t.x / k, t.y / k, t.z / k, t.w / k}
+}
+
+func (t Tuple) Magnitude() float64 {
+	return math.Sqrt(t.x * t.x + t.y * t.y + t.z * t.z + t.w * t.w)
 }
--- go-raytracer/tuple_test.go
+++ go-raytracer/tuple_test.go
@@ -1,6 +1,9 @@
 package goraytracer
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 
 func TestPointCreation(t *testing.T) {
@@ -16,5 +19,65 @@ func TestVectorCreation(t *testing.T) {
 
 	if vector.w != 0.0 {
 		t.Error("W coordinate of vector must be 0.0");
+	}
+}
+
+func TestTupleAddition(t *testing.T) {
+	t1 := Point(3, -2, 5)
+	t2 := Vector(-2, 3, 1)
+	ans := Tuple{1, 1, 6, 1}
+
+	add := t1.Add(t2)
+
+	if add != ans {
+		t.Error("Addition got wrong")
+	}
+}
+
+func TestTupleSubtraction(t *testing.T) {
+	t1 := Point(3, 2, 1)
+	t2 := Vector(5, 6, 7)
+	ans := Tuple{-2, -4, -6, 1}
+
+	sub := t1.Sub(t2)
+
+	if sub != ans {
+		t.Error("Subtraction got wrong")
+	}
+}
+
+func TestTupleNegation(t *testing.T) {
+	a := Tuple{1, -2, 3, -4}
+	ans := Tuple{-1, 2, -3, 4}
+
+	if a.Neg() != ans {
+		t.Error("Negation got wrong")
+	}
+}
+
+func TestTupleMultiplication(t *testing.T) {
+	a := Tuple{1, -2, 3, -4}
+	ans := Tuple{3.5, -7, 10.5, -14}
+
+	if a.Mul(3.5) != ans {
+		t.Error("Multiplicaion got wrong")
+	}
+}
+
+func TestTupleDivision(t *testing.T) {
+	a := Tuple{1, -2, 3, -4}
+	ans := Tuple{0.5, -1, 1.5, -2}
+
+	if a.Div(2) != ans {
+		t.Error("Multiplicaion got wrong")
+	}
+}
+
+func TestTupleMagnitude(t *testing.T) {
+	v := Vector(-1, -2, -3)
+	ans := math.Sqrt(14)
+
+	if v.Magnitude() != ans {
+		t.Error("Magnitude got wrong")
 	}
 }
