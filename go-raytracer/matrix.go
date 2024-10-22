56835bfc65b4e05bebcfcd49b48571f6de9f8c09
thecatblake <k222ryousuke@gmail.com>
2024-10-22 12:39:20 +0900

    add matrix

--- go-raytracer/matrix.go
+++ go-raytracer/matrix.go
@@ -6,6 +6,17 @@ type Mat4x4 struct {
 	data [4][4]float64
 }
 
+func NewIdentity() *Mat4x4 {
+	return &Mat4x4{
+		[4][4]float64{
+			{1, 0, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 1, 0},
+			{0, 0, 0, 1},
+		},
+	}
+}
+
 func (left *Mat4x4) Equals(right *Mat4x4) bool {
 	flag := true
 
@@ -20,33 +31,142 @@ func (left *Mat4x4) Equals(right *Mat4x4) bool {
 	return flag
 }
 
-func (left *Mat4x4) Mul(right *Mat4x4) Mat4x4 {
-	return Mat4x4{
+func (left *Mat4x4) Mul(right *Mat4x4) *Mat4x4 {
+	return &Mat4x4{
 		[4][4]float64{
 			{
 				left.data[0][0] * right.data[0][0] + left.data[0][1] * right.data[1][0] + left.data[0][2] * right.data[2][0] + left.data[0][3] * right.data[3][0],
-				left.data[0][0] * right.data[0][2] + right.data[0][1] * right.data[1][1] + left.data[0][2] * right.data[2][1] + left.data[0][3] * right.data[3][1],
-				left.data[0][0] * right.data[0][2] + right.data[0][2] * right.data[1][2] + left.data[0][2] * right.data[2][2] + left.data[0][3] * right.data[3][2],
-				left.data[0][0] * right.data[0][3] + right.data[0][3] * right.data[1][3] + left.data[0][2] * right.data[2][3] + left.data[0][3] * right.data[3][3],
+				left.data[0][0] * right.data[0][1] + left.data[0][1] * right.data[1][1] + left.data[0][2] * right.data[2][1] + left.data[0][3] * right.data[3][1],
+				left.data[0][0] * right.data[0][2] + left.data[0][1] * right.data[1][2] + left.data[0][2] * right.data[2][2] + left.data[0][3] * right.data[3][2],
+				left.data[0][0] * right.data[0][3] + left.data[0][1] * right.data[1][3] + left.data[0][2] * right.data[2][3] + left.data[0][3] * right.data[3][3],
 			},
 			{
 				left.data[1][0] * right.data[0][0] + left.data[1][1] * right.data[1][0] + left.data[1][2] * right.data[2][0] + left.data[1][3] * right.data[3][0],
-				left.data[1][0] * right.data[0][2] + left.data[1][1] * right.data[1][1] + left.data[1][2] * right.data[2][1] + left.data[1][3] * right.data[3][1],
-				left.data[1][0] * right.data[0][2] + left.data[1][2] * right.data[1][2] + left.data[1][2] * right.data[2][2] + left.data[1][3] * right.data[3][2],
-				left.data[1][0] * right.data[0][3] + left.data[1][3] * right.data[1][3] + left.data[1][2] * right.data[2][3] + left.data[1][3] * right.data[3][3],
+				left.data[1][0] * right.data[0][1] + left.data[1][1] * right.data[1][1] + left.data[1][2] * right.data[2][1] + left.data[1][3] * right.data[3][1],
+				left.data[1][0] * right.data[0][2] + left.data[1][1] * right.data[1][2] + left.data[1][2] * right.data[2][2] + left.data[1][3] * right.data[3][2],
+				left.data[1][0] * right.data[0][3] + left.data[1][1] * right.data[1][3] + left.data[1][2] * right.data[2][3] + left.data[1][3] * right.data[3][3],
 			},
 			{
 				left.data[2][0] * right.data[0][0] + left.data[2][1] * right.data[1][0] + left.data[2][2] * right.data[2][0] + left.data[2][3] * right.data[3][0],
-				left.data[2][0] * right.data[0][2] + left.data[2][1] * right.data[1][1] + left.data[2][2] * right.data[2][1] + left.data[2][3] * right.data[3][1],
-				left.data[2][0] * right.data[0][2] + left.data[2][2] * right.data[1][2] + left.data[2][2] * right.data[2][2] + left.data[2][3] * right.data[3][2],
-				left.data[2][0] * right.data[0][3] + left.data[2][3] * right.data[1][3] + left.data[2][2] * right.data[2][3] + left.data[2][3] * right.data[3][3],
+				left.data[2][0] * right.data[0][1] + left.data[2][1] * right.data[1][1] + left.data[2][2] * right.data[2][1] + left.data[2][3] * right.data[3][1],
+				left.data[2][0] * right.data[0][2] + left.data[2][1] * right.data[1][2] + left.data[2][2] * right.data[2][2] + left.data[2][3] * right.data[3][2],
+				left.data[2][0] * right.data[0][3] + left.data[2][1] * right.data[1][3] + left.data[2][2] * right.data[2][3] + left.data[2][3] * right.data[3][3],
 			},
 			{
 				left.data[3][0] * right.data[0][0] + left.data[3][1] * right.data[1][0] + left.data[3][2] * right.data[2][0] + left.data[3][3] * right.data[3][0],
-				left.data[3][0] * right.data[0][2] + left.data[3][1] * right.data[1][1] + left.data[3][2] * right.data[2][1] + left.data[3][3] * right.data[3][1],
-				left.data[3][0] * right.data[0][2] + left.data[3][2] * right.data[1][2] + left.data[3][2] * right.data[2][2] + left.data[3][3] * right.data[3][2],
-				left.data[3][0] * right.data[0][3] + left.data[3][3] * right.data[1][3] + left.data[3][2] * right.data[2][3] + left.data[3][3] * right.data[3][3],
+				left.data[3][0] * right.data[0][1] + left.data[3][1] * right.data[1][1] + left.data[3][2] * right.data[2][1] + left.data[3][3] * right.data[3][1],
+				left.data[3][0] * right.data[0][2] + left.data[3][1] * right.data[1][2] + left.data[3][2] * right.data[2][2] + left.data[3][3] * right.data[3][2],
+				left.data[3][0] * right.data[0][3] + left.data[3][1] * right.data[1][3] + left.data[3][2] * right.data[2][3] + left.data[3][3] * right.data[3][3],
 			},
 		},
 	}
+}
+
+func (m *Mat4x4) T() *Mat4x4 {
+	return &Mat4x4{
+		[4][4]float64{
+			{m.data[0][0], m.data[1][0], m.data[2][0], m.data[3][0]},
+			{m.data[0][1], m.data[1][1], m.data[2][1], m.data[3][1]},
+			{m.data[0][2], m.data[1][2], m.data[2][2], m.data[3][2]},
+			{m.data[0][3], m.data[1][3], m.data[2][3], m.data[3][3]},
+		},
+	}
+}
+
+func (m *Mat4x4) Inv() *Mat4x4 {
+	dst := [4][4]float64{}
+    /* Compute adjoint: */
+
+    dst[0][0] = m.data[1][1]*m.data[2][2]*m.data[3][3] - m.data[1][1]*m.data[2][3]*m.data[3][2] -
+		m.data[2][1]*m.data[1][2]*m.data[3][3] + m.data[2][1]*m.data[1][3]*m.data[3][2] +
+		m.data[3][1]*m.data[1][2]*m.data[2][3] - m.data[3][1]*m.data[1][3]*m.data[2][2]
+
+	dst[0][1] = -m.data[0][1]*m.data[2][2]*m.data[3][3] + m.data[0][1]*m.data[2][3]*m.data[3][2] +
+		m.data[2][1]*m.data[0][2]*m.data[3][3] - m.data[2][1]*m.data[0][3]*m.data[3][2] -
+		m.data[3][1]*m.data[0][2]*m.data[2][3] + m.data[3][1]*m.data[0][3]*m.data[2][2]
+
+	dst[0][2] = m.data[0][1]*m.data[1][2]*m.data[3][3] - m.data[0][1]*m.data[1][3]*m.data[3][2] -
+		m.data[1][1]*m.data[0][2]*m.data[3][3] + m.data[1][1]*m.data[0][3]*m.data[3][2] +
+		m.data[3][1]*m.data[0][2]*m.data[1][3] - m.data[3][1]*m.data[0][3]*m.data[1][2]
+
+	dst[0][3] = -m.data[0][1]*m.data[1][2]*m.data[2][3] + m.data[0][1]*m.data[1][3]*m.data[2][2] +
+		m.data[1][1]*m.data[0][2]*m.data[2][3] - m.data[1][1]*m.data[0][3]*m.data[2][2] -
+		m.data[2][1]*m.data[0][2]*m.data[1][3] + m.data[2][1]*m.data[0][3]*m.data[1][2]
+
+	dst[1][0] = -m.data[1][0]*m.data[2][2]*m.data[3][3] + m.data[1][0]*m.data[2][3]*m.data[3][2] +
+		m.data[2][0]*m.data[1][2]*m.data[3][3] - m.data[2][0]*m.data[1][3]*m.data[3][2] -
+		m.data[3][0]*m.data[1][2]*m.data[2][3] + m.data[3][0]*m.data[1][3]*m.data[2][2]
+
+	dst[1][1] = m.data[0][0]*m.data[2][2]*m.data[3][3] - m.data[0][0]*m.data[2][3]*m.data[3][2] -
+		m.data[2][0]*m.data[0][2]*m.data[3][3] + m.data[2][0]*m.data[0][3]*m.data[3][2] +
+		m.data[3][0]*m.data[0][2]*m.data[2][3] - m.data[3][0]*m.data[0][3]*m.data[2][2]
+
+	dst[1][2] = -m.data[0][0]*m.data[1][2]*m.data[3][3] + m.data[0][0]*m.data[1][3]*m.data[3][2] +
+		m.data[1][0]*m.data[0][2]*m.data[3][3] - m.data[1][0]*m.data[0][3]*m.data[3][2] -
+		m.data[3][0]*m.data[0][2]*m.data[1][3] + m.data[3][0]*m.data[0][3]*m.data[1][2]
+
+	dst[1][3] = m.data[0][0]*m.data[1][2]*m.data[2][3] - m.data[0][0]*m.data[1][3]*m.data[2][2] -
+		m.data[1][0]*m.data[0][2]*m.data[2][3] + m.data[1][0]*m.data[0][3]*m.data[2][2] +
+		m.data[2][0]*m.data[0][2]*m.data[1][3] - m.data[2][0]*m.data[0][3]*m.data[1][2]
+
+	dst[2][0] = m.data[1][0]*m.data[2][1]*m.data[3][3] - m.data[1][0]*m.data[2][3]*m.data[3][1] -
+		m.data[2][0]*m.data[1][1]*m.data[3][3] + m.data[2][0]*m.data[1][3]*m.data[3][1] +
+		m.data[3][0]*m.data[1][1]*m.data[2][3] - m.data[3][0]*m.data[1][3]*m.data[2][1]
+
+	dst[2][1] = -m.data[0][0]*m.data[2][1]*m.data[3][3] + m.data[0][0]*m.data[2][3]*m.data[3][1] +
+		m.data[2][0]*m.data[0][1]*m.data[3][3] - m.data[2][0]*m.data[0][3]*m.data[3][1] -
+		m.data[3][0]*m.data[0][1]*m.data[2][3] + m.data[3][0]*m.data[0][3]*m.data[2][1]
+
+	dst[2][2] = m.data[0][0]*m.data[1][1]*m.data[3][3] - m.data[0][0]*m.data[1][3]*m.data[3][1] -
+		m.data[1][0]*m.data[0][1]*m.data[3][3] + m.data[1][0]*m.data[0][3]*m.data[3][1] +
+		m.data[3][0]*m.data[0][1]*m.data[1][3] - m.data[3][0]*m.data[0][3]*m.data[1][1]
+
+	dst[2][3] = -m.data[0][0]*m.data[1][1]*m.data[2][3] + m.data[0][0]*m.data[1][3]*m.data[2][1] +
+		m.data[1][0]*m.data[0][1]*m.data[2][3] - m.data[1][0]*m.data[0][3]*m.data[2][1] -
+		m.data[2][0]*m.data[0][1]*m.data[1][3] + m.data[2][0]*m.data[0][3]*m.data[1][1]
+
+	dst[3][0] = -m.data[1][0]*m.data[2][1]*m.data[3][2] + m.data[1][0]*m.data[2][2]*m.data[3][1] +
+		m.data[2][0]*m.data[1][1]*m.data[3][2] - m.data[2][0]*m.data[1][2]*m.data[3][1] -
+		m.data[3][0]*m.data[1][1]*m.data[2][2] + m.data[3][0]*m.data[1][2]*m.data[2][1]
+
+	dst[3][1] = m.data[0][0]*m.data[2][1]*m.data[3][2] - m.data[0][0]*m.data[2][2]*m.data[3][1] -
+		m.data[2][0]*m.data[0][1]*m.data[3][2] + m.data[2][0]*m.data[0][2]*m.data[3][1] +
+		m.data[3][0]*m.data[0][1]*m.data[2][2] - m.data[3][0]*m.data[0][2]*m.data[2][1]
+
+	dst[3][2] = -m.data[0][0]*m.data[1][1]*m.data[3][2] + m.data[0][0]*m.data[1][2]*m.data[3][1] +
+		m.data[1][0]*m.data[0][1]*m.data[3][2] - m.data[1][0]*m.data[0][2]*m.data[3][1] -
+		m.data[3][0]*m.data[0][1]*m.data[1][2] + m.data[3][0]*m.data[0][2]*m.data[1][1]
+
+	dst[3][3] = m.data[0][0]*m.data[1][1]*m.data[2][2] - m.data[0][0]*m.data[1][2]*m.data[2][1] -
+		m.data[1][0]*m.data[0][1]*m.data[2][2] + m.data[1][0]*m.data[0][2]*m.data[2][1] +
+		m.data[2][0]*m.data[0][1]*m.data[1][2] - m.data[2][0]*m.data[0][2]*m.data[1][1]
+
+	// Compute determinant
+	det := m.data[0][0]*dst[0][0] + m.data[0][1]*dst[1][0] + m.data[0][2]*dst[2][0] + m.data[0][3]*dst[3][0]
+
+
+    /* Multiply adjoint with reciprocal of determinant: */
+
+    det = 1.0 / det;
+
+    dst[0][0] *= det;
+    dst[0][1] *= det;
+    dst[0][2] *= det;
+    dst[0][3] *= det;
+    dst[1][0] *= det;
+    dst[1][1] *= det;
+    dst[1][2] *= det;
+    dst[1][3] *= det;
+    dst[2][0] *= det;
+    dst[2][1] *= det;
+    dst[2][2] *= det;
+    dst[2][3] *= det;
+    dst[3][0] *= det;
+    dst[3][1] *= det;
+    dst[3][2] *= det;
+    dst[3][3] *= det;
+
+    return &Mat4x4{
+        dst,
+    }
 }
--- go-raytracer/matrix_test.go
+++ go-raytracer/matrix_test.go
@@ -1,6 +1,8 @@
 package goraytracer
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMatrixEquality(t *testing.T) {
 	a := Mat4x4{
@@ -9,7 +11,7 @@ func TestMatrixEquality(t *testing.T) {
             {0, 1, 2, 3},
             {0, 1, 2.111, 3},
             {0.111, 1, 2.11, 3},
-        }
+        },
     }
 
     b := Mat4x4{
@@ -18,6 +20,106 @@ func TestMatrixEquality(t *testing.T) {
             {0, 1, 2, 3},
             {0, 1, 2.111, 3},
             {0.111, 1, 2.11, 3},
-        }
+        },
+    }
+
+    if !a.Equals(&b) {
+        t.Error("Matrix equality got wrong")
+    }
+}
+
+func TestMatrixMultiplication(t *testing.T) {
+	a := Mat4x4{
+        [4][4]float64{
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 8, 7, 6},
+            {5, 4, 3, 2},
+        },
+    }
+
+    b := Mat4x4{
+        [4][4]float64{
+            {-2, 1, 2, 3},
+            {3, 2, 1, -1},
+            {4, 3, 6, 5},
+            {1, 2, 7, 8},
+        },
+    }
+    
+    c := Mat4x4{
+        [4][4]float64{
+            {20, 22, 50, 48},
+            {44, 54, 114, 108},
+            {40, 58, 110, 102},
+            {16, 26, 46, 42},
+        },
+    }
+
+    mul := a.Mul(&b)
+
+    if !mul.Equals(&c) {
+        t.Error("Matrix multiplication got wrong")
+    }
+}
+
+func TestIdentity(t *testing.T) {
+	a := Mat4x4{
+        [4][4]float64{
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 8, 7, 6},
+            {5, 4, 3, 2},
+        },
+    }
+
+    b := NewIdentity()
+
+    mul1 := a.Mul(b)
+    mul2 := b.Mul(&a)
+
+    if !mul1.Equals(&a) && !mul2.Equals(&a) {
+        t.Error("Identity got wrong")
+    }
+}
+
+func TestTranspose(t *testing.T) {
+    a := Mat4x4{
+        [4][4]float64{
+            {0, 9, 3, 0},
+            {9, 8, 0, 8},
+            {1, 8, 5, 3},
+            {0, 0, 5, 8},
+        },
+    }
+
+    b := Mat4x4{
+        [4][4]float64{
+            {0, 9, 1, 0},
+            {9, 8, 8, 0},
+            {3, 0, 5, 5},
+            {0, 8, 3, 8},
+        },
+    }
+
+    c := a.T()
+
+    if !c.Equals(&b) {
+        t.Error("Transpose got wrong")
+    }
+}
+
+func TestMatrixInv(t *testing.T) {
+    a := Mat4x4{
+        [4][4]float64{
+            {0, 9, 3, 0},
+            {9, 8, 0, 8},
+            {1, 8, 5, 3},
+            {0, 0, 5, 8},
+        },
+    }
+
+    if !a.Mul(a.Inv()).Equals(NewIdentity()) || !a.Inv().Mul(&a).Equals(NewIdentity()) {
+        t.Error("Inverse got wrong")
     }
 }
